c255848b1d4c35fe5bdbfb6bf4a95a1db7063652
Cleucil Francisco <cleucilf@gmail.com>
2026-03-05 22:02:52 -0800

    Add pseudocode

---
+++ restaurantBill.c
@@ -0,0 +1,4 @@
+// Add standard C libraries and create main with command line args
+// Create variables such as: tax_amount, meal_cost, tip_amount,total_bill
+//Create a variable that will randomly choose from the 4 meals  
+// Output tax_amount, meal_cost, tip_amount,total_bill
