aa1530d8ffd605313b184f9a6edc17334c9f3cb3
Cleucil Francisco <cleucilf@gmail.com>
2026-03-06 11:03:02 -0800

    Outline of numberGuessGame.c

---
+++ numberGuess.c
@@ -0,0 +1,4 @@
+//Stage 1: Create a small version of the program that includes: 
+//input from user: int, output: string (right or wrong), and random number generator.
+//Stage 2: Add constraints (input validation), menu of options with only 1 (with low or high suggestions) and 3 functioning
+// Stage 3: Add file ops
